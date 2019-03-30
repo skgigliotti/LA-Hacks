b3bf49df5da29a32ee7d914a941844a7fae0f3b5
greeeen89 <greeeenmango89@gmail.com>
2019-03-30 16:27:30 -0700

    add: authentication for google calendar

--- send_rec.py
+++ send_rec.py
@@ -6,8 +6,18 @@ Created on Sat Mar 30 10:58:31 2019
 @author: Dempsey
 """
 
+from __future__ import print_function
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 app = Flask(__name__)
 
@@ -26,11 +36,49 @@ def sms_ahoy_reply():
 
 """ Purpose: Allows student/requester to schedule an appointment. """
 def schedule_appointment():
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
     return()
 
 """ Purpose: Allows program to modify advisor's calender. """
 def authenticate():
-    return()
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return(creds)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    creds = authenticate() # get credentials for google account
+    service = build('calendar', 'v3', credentials=creds) # manipulate google calendar
+    schedule_appointment()
+    #app.run(debug=True)
